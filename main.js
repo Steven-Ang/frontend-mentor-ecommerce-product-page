81226372e3f9092c14dff9b9ec7fbdf1e0e86956
Steven Ang <spikegreen2@gmail.com>
2024-10-17 21:57:35 +0800

    Add content

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const closeMenuButton = document.getElementById("close-menu-button");
 const navigationOverlay = document.getElementById("navigation-overlay");
 const navigationContent = document.getElementById("navigation-content");
 
+const minusQuantityButton = document.getElementById("minus-quantity-button");
+const plusQuantityButton = document.getElementById("plus-quantity-button");
+const quantity = document.getElementById("quantity");
+
 const media = window.matchMedia("(width < 52em)");
 
 const openMenu = () => {
@@ -25,6 +29,16 @@ const closeMenu = () => {
   openMenuButton.focus();
 };
 
+const handleMinusButtonClick = (quantity) => {
+  const quantityAmout = Number(quantity.textContent);
+  if (quantityAmout > 0) quantity.textContent = quantityAmout - 1;
+};
+
+const handlePlusButtonClick = (quantity) => {
+  const quantityAmout = Number(quantity.textContent);
+  quantity.textContent = quantityAmout + 1;
+};
+
 const handleResize = (event) => {
   if (event.matches) {
     openMenuButton.setAttribute("aria-expanded", "false");
@@ -35,4 +49,12 @@ const handleResize = (event) => {
 
 openMenuButton.addEventListener("click", openMenu);
 closeMenuButton.addEventListener("click", closeMenu);
+
+minusQuantityButton.addEventListener("click", () =>
+  handleMinusButtonClick(quantity)
+);
+plusQuantityButton.addEventListener("click", () =>
+  handlePlusButtonClick(quantity)
+);
+
 media.addEventListener("change", handleResize);

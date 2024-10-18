469da4a39e74c57205ff02920659374385e90a35
Steven Ang <spikegreen2@gmail.com>
2024-10-19 00:30:21 +0800

    Add carousel

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const closeMenuButton = document.getElementById("close-menu-button");
 const navigationOverlay = document.getElementById("navigation-overlay");
 const navigationContent = document.getElementById("navigation-content");
 
+const carouselButtons = document.querySelectorAll("[data-carousel-button]");
+const productThumbnailImageButtons = document.querySelectorAll(
+  ".product-thumbnail-image-button"
+);
+
 const minusQuantityButton = document.getElementById("minus-quantity-button");
 const plusQuantityButton = document.getElementById("plus-quantity-button");
 const quantity = document.getElementById("quantity");
@@ -29,6 +34,37 @@ const closeMenu = () => {
   openMenuButton.focus();
 };
 
+const handleCarouselButton = (event) => {
+  const offset = event.currentTarget.dataset.carouselButton === "next" ? 1 : -1;
+
+  const slides = event.target
+    .closest("[data-carousel]")
+    .querySelector("[data-slides]");
+
+  const activeSlide = slides.querySelector("[data-active]");
+  let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+  if (newIndex < 0) newIndex = slides.children.length - 1;
+  if (newIndex >= slides.children.length) newIndex = 0;
+
+  slides.children[newIndex].dataset.active = true;
+  delete activeSlide.dataset.active;
+};
+
+const handleProductThumbnailImageButtonClick = (event) => {
+  const button = event.target.parentNode;
+
+  const imagesList = button.closest("[data-product-thumbnail-images");
+
+  const activeElement = imagesList.querySelector("[data-active]");
+
+  console.log(activeElement === button);
+
+  if (activeElement !== button) {
+    button.dataset.active = true;
+    delete activeElement.dataset.active;
+  }
+};
+
 const handleMinusButtonClick = (quantity) => {
   const quantityAmout = Number(quantity.textContent);
   if (quantityAmout > 0) quantity.textContent = quantityAmout - 1;
@@ -49,6 +85,18 @@ const handleResize = (event) => {
 
 openMenuButton.addEventListener("click", openMenu);
 closeMenuButton.addEventListener("click", closeMenu);
+
+carouselButtons.forEach((carouselButton) =>
+  carouselButton.addEventListener("click", (event) =>
+    handleCarouselButton(event)
+  )
+);
+
+productThumbnailImageButtons.forEach((productThumbnailImageButton) => {
+  productThumbnailImageButton.addEventListener("click", (event) =>
+    handleProductThumbnailImageButtonClick(event)
+  );
+});
 
 minusQuantityButton.addEventListener("click", () =>
   handleMinusButtonClick(quantity)
